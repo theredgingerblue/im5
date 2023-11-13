18dab9921c991c096feb900b417cc09dc9785647
theredgingerblue <miriam.goldmann@stud.fhgr.ch>
2023-11-13 10:57:06 +0100

    upload files

---
+++ app.js
@@ -0,0 +1,27 @@
+// JavaScript in "script.js"
+const smallImages = document.querySelectorAll(".small-image");
+const imageModal = document.getElementById("image-modal");
+const largeImage = document.getElementById("large-image");
+const closeButton = document.getElementById("close-button");
+
+smallImages.forEach((image) => {
+    image.addEventListener("click", () => {
+        imageModal.style.display = "block";
+        largeImage.src = image.getAttribute("data-large");
+    });
+});
+
+closeButton.addEventListener("click", () => {
+    imageModal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === imageModal) {
+        imageModal.style.display = "none";
+    }
+});
+
+//Zurück-Button auf den Seiten, die auf die jeweils vorherige Seite zurückleitet.
+const zurueckButton = document.getElementById("zurueckButton");
+
+
